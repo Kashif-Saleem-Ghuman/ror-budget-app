d8fd828e70fc2a5e57f02a4b246ab1ef287a5904
unknown <kashif_saleem_23@hotmail.com>
2023-04-26 18:53:02 +0500

    Modify models

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,14 @@
 class Group < ApplicationRecord
   belongs_to :author, class_name: 'User'
+  has_many :group_purchases, dependent: :destroy
   has_many :purchases, through: :group_purchases
+
+  has_one_attached :icon
+  validates :name, :icon, presence: true
+
+  after_destroy :total_purchases_group_delete
+
+  def total_purchases_group_delete
+    group.decrement!(:total_purchases, purchase.amount)
+  end
 end
--- app/models/group_purchase.rb
+++ app/models/group_purchase.rb
@@ -1,4 +1,10 @@
 class GroupPurchase < ApplicationRecord
   belongs_to :group
   belongs_to :purchase
+
+  after_create :total_purchases_group_add
+
+  def total_purchases_group_add
+    group.increment!(:total_purchases, purchase.amount)
+  end
 end
