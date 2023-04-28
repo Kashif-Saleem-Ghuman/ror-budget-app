e693699adb22453e7f36d46e685672ff7ae500ea
unknown <kashif_saleem_23@hotmail.com>
2023-04-28 23:11:16 +0500

    Add tests for group index to test everything

--- spec/views/groups_index_spec.rb
+++ spec/views/groups_index_spec.rb
@@ -5,27 +5,45 @@ RSpec.describe 'categories index', type: :feature do
     before(:each) do
       User.destroy_all
       Group.destroy_all
-      @u1 = User.new(name: 'Anna', password: '123456', email: 'anna@gmail.com')
-      @u1.save
-      @r1 = Group.new(id: 4, name: 'Rent', icon: Rack::Test::UploadedFile.new('spec/support/restaurant.png', 'restaurant/png'),
-                      total_purchases: 123)
-      @r1.save
+      @u1 = User.create(name: 'Anna', password: '123456', email: 'anna@gmail.com')
       visit new_user_session_path
       fill_in 'Email', with: 'anna@gmail.com'
       fill_in 'password', with: '123456'
       click_button 'Log in'
     end
-    it 'shoud show the categories page' do
+
+    it 'should show the categories page' do
       expect(page).to have_content('CATEGORIES')
     end
-    it 'should show the name of all my categories' do
-      expect(page).to have_content('sign out')
-    end
-    it 'should show the total amount of each category' do
-      expect(page).to have_content('CATEGORIES')
-    end
-    it 'should have a button to add categories' do
-      expect(page).to have_content('Add new Category')
+
+    context 'when categories exist' do
+      before(:each) do
+        visit new_group_path
+        fill_in 'Name', with: 'Rent'
+        attach_file('group_icon', Rails.root.join('spec', 'support', 'restaurant.png'))
+        click_button 'Add'
+      end
+
+      it 'should show the name of all my categories' do
+        expect(page).to have_content('Rent')
+      end
+
+
+      it 'should have a button to add categories' do
+        expect(page).to have_content('Add new Category')
+      end
+
+      it 'should have a button to delete categories' do
+        expect(page).to have_content('Delete')
+      end
+
+      it 'should have date of creation' do
+        expect(page).to have_content(Date.today.strftime('%d %b %Y'))
+      end
+
+      it 'should have the icon of the category' do
+        expect(page).to have_css("img[src*='restaurant.png']")
+      end
     end
   end
 end
